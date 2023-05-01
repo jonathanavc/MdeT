95232d2e665f8fa33af7dc01bb79496e5654af17
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-01 19:01:35 -0400

    Update TNF.cu

--- cuda/TNF.cu
+++ cuda/TNF.cu
@@ -69,7 +69,7 @@ __global__ void get_TNF(double * TNF_d , const char * seqs_d, const size_t * seq
         size_t contig_index = (blockIdx.x * contigs_per_thread) + i;
         if(contig_index >= nobs) break;
         if(smallCtgs[contig_index] == 0){
-            const char * contig = get_contig_d(gCtgCtgIdx_dgIdx[contig_index], seqs_d, seqs_d_index);
+            const char * contig = get_contig_d(gCtgIdx_d[contig_index], seqs_d, seqs_d_index);
             int contig_size = seqs_d_index[gCtgIdx_d[contig_index]];
             if(gCtgIdx_d[contig_index] != 0){
                 contig_size -= seqs_d_index[gCtgIdx_d[contig_index] - 1];
