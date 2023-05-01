e79cc9f620358226870742beddc5fe60bcb562c6
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-01 13:40:06 -0400

    si

--- cuda/TNF.cu
+++ cuda/TNF.cu
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <stdgpu/unordered_map.cuh>     // stdgpu::unordered_map
-#include <stdgpu/unordered_set.cuh>     // stdgpu::unordered_set
+#include "extra/KseqReader.h"
 
-__device__ int n_TNF = 136;
+__device__ __host__ const int n_TNF = 136;
+__device__ __host__ const int n_TNFP = 16;
 
-__global__ static const std::string TN[] = { "GGTA", "AGCC", "AAAA", "ACAT", "AGTC", "ACGA", "CATA", "CGAA", "AAGT", "CAAA",
+static const std::string TN[] = { "GGTA", "AGCC", "AAAA", "ACAT", "AGTC", "ACGA", "CATA", "CGAA", "AAGT", "CAAA",
         "CCAG", "GGAC", "ATTA", "GATC", "CCTC", "CTAA", "ACTA", "AGGC", "GCAA", "CCGC", "CGCC", "AAAC", "ACTC", "ATCC",
 		"GACC", "GAGA", "ATAG", "ATCA", "CAGA", "AGTA", "ATGA", "AAAT", "TTAA", "TATA", "AGTG", "AGCT", "CCAC", "GGCC",
 		"ACCC", "GGGA", "GCGC", "ATAC", "CTGA", "TAGA", "ATAT", "GTCA", "CTCC", "ACAA", "ACCT", "TAAA", "AACG", "CGAG",
@@ -15,14 +15,9 @@ __global__ static const std::string TN[] = { "GGTA", "AGCC", "AAAA", "ACAT", "AG
 		"GAAC", "GTAA", "ATGC", "GTTA", "TCCA", "CAGG", "ACTG", "AAAG", "AAGA", "CAAG", "GCGA", "AACC", "ACGG", "CCAA",
 		"CTTA", "AGAC", "AGCG", "GAAA", "AATC", "ATTG", "GCAC", "CCTA", "CGAC", "CTAG", "AGAA", "CGCA", "CGCG", "AATA" };
 
-__global__ static const std::string TNP[] = { "ACGT", "AGCT", "TCGA", "TGCA", "CATG", "CTAG", "GATC", "GTAC", "ATAT", "TATA","CGCG",
+
+static const std::string TNP[] = { "ACGT", "AGCT", "TCGA", "TGCA", "CATG", "CTAG", "GATC", "GTAC", "ATAT", "TATA","CGCG",
         "GCGC", "AATT", "TTAA", "CCGG", "GGCC" };
-
-//__device__ static stdgpu::unordered_map<int, int> TNmap;
-//__device__ static stdgpu::unordered_map<size_t, size_t> gCtgIdx;
-//__device__ static stdgpu::unordered_set<int> TNPmap;
-//__device__ static stdgpu::unordered_set<size_t> smallCtgs
-
 
 __device__ char * get_contig_d(int contig_index,char * seqs_d, int * seqs_d_index){
     size_t contig_beg = 0;
@@ -37,26 +32,37 @@ __device__ char * get_contig_d(int contig_index,char * seqs_d, int * seqs_d_inde
     }
 }
 
-__device__ int get_tn_d(char * contig, int index){
-    int tn = 0;
-    for(int i = 0; i<4; i++){
-        tn+= tn<<8 + contig[index + i];
+__device__ __host__ unsigned char get_tn(char * contig, int index){
+    unsigned char tn = 0;
+    for(int i = 3; i >= 0; i--){
+        char N = contig[index + i];
+        if (N == 'A')
+			N = 0;
+		else if (N == 'C')
+			N = 1;
+		else if (N == 'T')
+			N = 2;
+		else if (s[i] == 'G')
+		    N = 3;
+        else
+            return 0;
+        tn += tn<<2 + N;
     }
     return tn;
 }
 
-__device__ int get_revComp_tn_d(int contig_index,char * seqs_d, int * seqs_d_index){
-    int tn = 0;
+__device__ unsigned char get_revComp_tn_d(int contig_index,char * seqs_d, int * seqs_d_index){
+    unsigned char tn = 0;
     for(int i = 3; i >= 0; i--){
         char N = contig[index + i];
         if (N == 'A')
-			N = 'T';
-		else if (N == 'T')
-			N = 'A';
+			N = 2;
 		else if (N == 'C')
-			N = 'G';
+			N = 3;
+		else if (N == 'T')
+			N = 0;
 		else if (s[i] == 'G')
-		    N = 'C';
+		    N = 1;
         else
             return 0;
         tn+= tn<<8 + N;
@@ -64,34 +70,9 @@ __device__ int get_revComp_tn_d(int contig_index,char * seqs_d, int * seqs_d_ind
     return tn;
 }
 
-__global__ void TNF(double * TNF_d , char * seqs_d, size_t * seqs_d_index , size_t nobs,
-    const stdgpu::unordered_map<int, int> TNmap,
-    const stdgpu::unordered_set<int> TNPmap,
-    const stdgpu::unordered_set<size_t> smallCtgs,
-    const stdgpu::unordered_map<size_t, size_t> gCtgIdx,
-    /*
-    size_t * smallCtgs_KEY, size_t smallCtgs_size, 
-    size_t * gCtgIdx_KEY, size_t * gCtgIdx_VALUE, size_t gCtgIdx_size,
-    */
-    size_t contigs_per_thread){
-
-    // crear mapas y sets
-    /*
-    if(blockIdx.x == 0){
-        for(size_t = 0; i < smallCtgs_size){
-            smallCtgs
-        }
-        for(size_t i = 0; i < n_TNF; ++i) {
-            TNmap[TN[i][0]<<24 + TN[i][1]<<16 + TN[i][2]<<8 + TN[i][3]] = i;
-        }
-        for(size_t i = 0; i < 16; ++i) {
-            TNPmap[TNP[i][0]<<24 + TNP[i][1]<<16 + TNP[i][2]<<8 + TNP[i][3]] = i;
-        }
-
-    }
-    __syncthreads();
-    */
-
+__global__ void TNF(double * TNF_d , const char * seqs_d, const size_t * seqs_d_index , size_t nobs,
+    const unsigned char * TNmap, const unsigned char * TNPmap, const unsigned char * smallCtgs,
+    const size_t * gCtgIdx,size_t contigs_per_thread){
     // inicializar valores de vector en 0
     for(int i = 0, i < contigs_per_thread){ 
         int contig_index = (blockIdx.x * contigs_per_thread) + i;
@@ -106,27 +87,25 @@ __global__ void TNF(double * TNF_d , char * seqs_d, size_t * seqs_d_index , size
     for(size_t i = 0, i < contigs_per_thread){
         size_t contig_index = (blockIdx.x * contigs_per_thread) + i;
         if(contig_index >= nobs) break;
-        if(smallCtgs.find(contig_index) == smallCtgs.end()){
+        if(smallCtgs[contig_index] == 0){
             char * contig = get_contig_d(gCtgIdx[contig_index], seqs_d, seqs_d_index);
             int contig_size = seqs_d_index[gCtgIdx[contig_index]];
             if(gCtgIdx[contig_index] != 0){
                 contig_size -= seqs_d_index[gCtgIdx[contig_index] - 1];
             }
-            for (size_t i = 0; i < contig_size; ++i) {
-                int tn = get_contig_d(contig, i);
-                stdgpu::unordered_map<int, int>::iterator it = TNmap.find(tn);
+            for (size_t i = 0; i < contig_size - 3; ++i) {
+                unsigned char tn = get_tn(contig, i);
                 //SI tn NO SE ENCUENTRA EN TNmap el complemento del palindromo sí estará
-                if(it != TNmap.end()){
-                    ++TNF_d[contig_index * n_TNF + it->second];   
+                if(TNmap[tn] != -1){
+                    ++TNF_d[contig_index * n_TNF + TNmap[tn]];   
                 }
                 
                 tn = get_revComp_tn_d(contig, i);
 
                 //SALTA EL PALINDROMO PARA NO INSERTARLO NUEVAMENTE
-                if (TNPmap.find(tn) == TNPmap.end()) {
-                    it = TNmap.find(tn);
-                    if(it != TNmap.end()){
-                        ++TNF_d(contig_index * n_TNF + it->second);
+                if (TNPmap[tn] == 0) {
+                    if(TNmap[tn] != -1){
+                        ++TNF_d(contig_index * n_TNF + TNmap[tn]);
                     }
                 }
             }
@@ -145,15 +124,93 @@ __global__ void TNF(double * TNF_d , char * seqs_d, size_t * seqs_d_index , size
 }
 
 static int N_THREADS = 100;
-static std::unordered_map<std::string, int> TNmap;
-static std::unordered_set<std::string> TNPmap;
+std::vector<std::string> seqs;
+std::vector<size_t> gCtgIdx;
+std::vector<unsigned char> smallCtgs;
+unsigned char TNmap[256];
+unsigned char TNPmap[256];
+
+double * TNF_d;
+const char * seqs_d;
+const size_t * seqs_d_index;
+const unsigned char * TNmap_d,
+const unsigned char * TNPmap_D,
+const unsigned char * smallCtgs,
+const size_t * gCtgIdx_d;
+
+int _cudaMemcpy(void * _device_pointer, void * _host_pointer, size_t size, int type){
+    int err;
+    if(type == 1){
+        err += cudaMalloc(&_device_pointer, size);
+        err += cudaMemcpy(_device_pointer, _host_pointer, size, cudaMemcpyHostToDevice); 
+    }
+    return (err == 0) : 0 ? 1; 
+}
 
 int main(int argc, char const *argv[]){
-    for(size_t i = 0; i < n_TNF; ++i) {
-		TNmap[TN[i][0]<<24 + TN[i][1]<<16 + TN[i][2]<<8 + TN[i][3]] = i;
+    // se inicializan los mapas
+    for(int i = 0; i < 256; i++){
+        TNmap[i] = -1;
+        TNPmap[i] = 0;
+    }
+    for(int i = 0; i < n_TNF; ++i) {
+        unsigned char key = get_tn(TN[i]);
+        TNmap[key] = i;
 	}
-	for(size_t i = 0; i < 16; ++i) {
-		TNPmap[TNP[i][0]<<24 + TNP[i][1]<<16 + TNP[i][2]<<8 + TNP[i][3]] = i;
+	for(size_t i = 0; i < n_TNFP; ++i) {
+		unsigned char key = get_tn(TNP[i]);
+        TNmap[key] = 1;
+	}
+    
+	size_t nobs = 0;
+	int nresv = 0;
+	gzFile f = gzopen(inFile.c_str(), "r");
+	if (f == NULL) {
+		cerr << "[Error!] can't open the sequence fasta file " << inFile << endl;
+		return 1;
+	} else {
+		kseq_t * kseq = kseq_init(f);
+		int64_t len;
+		while ((len = kseq_read(kseq)) > 0) {
+			std::transform(kseq->seq.s, kseq->seq.s + len, kseq->seq.s, ::toupper);
+			if (kseq->name.l > 0) {
+				if(len >= (int) std::min(minContigByCorr, minContigByCorrForGraph)) {
+					if(len < (int) minContig) {
+						if(len >= (int) minContigByCorr)
+                            // cambio para facilitar la transferencia de smallCtgs
+                            smallCtgs.emplace_back(1);
+							//smallCtgs.insert(nobs);
+						else
+                            // cambio para facilitar la transferencia de smallCtgs
+                            smallCtgs.emplace_back(0);
+							++nresv;
+					}
+                    else
+                        smallCtgs.emplace_back(0);
+					lCtgIdx[kseq->name.s] = nobs;
+                    /////// cambio para facilitar la transferencia de gCtgIdx
+					gCtgIdx.emplace_back(seqs.size());
+                    nobs++;
+                    ///////
+                
+				} else
+					ignored[kseq->name.s] = seqs.size();
+				contig_names.push_back(kseq->name.s);
+				seqs.push_back(kseq->seq.s);
+			}
+		}
+		kseq_destroy(kseq);
+		kseq = NULL;
+		gzclose(f);
+	}
+
+	assert(nobs == lCtgIdx.size());
+	nobs2 = ignored.size();
+	verbose_message("Finished reading %d contigs. Number of target contigs >= %d are %d, and [%d and %d) are %d \n", nobs + nobs2, minContig, nobs - smallCtgs.size() - nresv, minContigByCorr, minContig, smallCtgs.size());
+
+	if(contig_names.size() != nobs + nobs2 || seqs.size() != nobs + nobs2) {
+		cerr << "[Error!] Need to check whether there are duplicated sequence ids in the assembly file" << endl;
+		return 1;
 	}
 
     std::string seqs_h;
@@ -163,12 +220,18 @@ int main(int argc, char const *argv[]){
         seqs_h_index.emplace_back(seqs_h.size());
     }
 
-    err = cudaMemcpy(seqs_d, combined.data(), combined.size(), cudaMemcpyHostToDevice);
-    err = cudaMemcpy(seqs_d_index, indexes.data(), indexes.size() * sizeof(size_t), cudaMemcpyHostToDevice);
+    cudaMalloc(&TNF, nobs * n_TNF * size_t(double))                                                                 // memoria para almacenar TNF
+    err = _cudaMemcpy(TNmap_d, TNmap, n_TNF, cudaMemcpyHostToDevice);                                               // TNmap
+    err = _cudaMemcpy(TNPmap_d, TNPmap, n_TNFP, cudaMemcpyHostToDevice);                                            // TNPmap 
+    err = _cudaMemcpy(seqs_d, seqs_h.data(), combined.size(), cudaMemcpyHostToDevice);                              // seqs
+    err = _cudaMemcpy(seqs_d_index, seqs_h_index.data(), indexes.size() * sizeof(size_t), cudaMemcpyHostToDevice);  // seqs_index
+    err = _cudaMemcpy(gCtgIdx_d, gCtgIdx.data(), nobs * sizeof(size_t), cudaMemcpyHostToDevice);                    // gCtgIdx
+    err = _cudaMemcpy(smallCtgs_d, smallCtgs.data(), nobs, cudaMemcpyHostToDevice);                                 // seqs
+
 
     size_t contigs_per_thread = 1 + ((nobs - 1) / N_THREADS);
 
-    TNF<<<1,N_THREADS>>>(TNF_d, seqs_d, seqs_d_index, nobs, TNmap, TNPmap, smallCtgs, gCtgIdx, contigs_per_thread);
+    TNF<<<1,N_THREADS>>>(TNF_d, seqs_d, seqs_d_index, nobs, TNmap_d, TNPmap_d, smallCtgs_d, gCtgIdx_d, contigs_per_thread);
 
     cudaDeviceSynchronize():
 
