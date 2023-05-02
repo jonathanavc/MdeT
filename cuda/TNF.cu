7c47b8a3d273cdb460e0ce8dff40c0c034d83d2f
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-01 20:46:03 -0400

    Update TNF.cu

--- cuda/TNF.cu
+++ cuda/TNF.cu
@@ -150,12 +150,12 @@ int main(int argc, char const *argv[]){
     }
     for(int i = 0; i < n_TNF; ++i) {
         unsigned char key = get_tn(TN[i].c_str(), 0);
-        std::cout << key <<" ";
+        std::cout << (int)key <<" ";
         TNmap[key] = i;
 	}
     std::cout << std::endl;
 	for(size_t i = 0; i < n_TNFP; ++i) {
-		unsigned char key = get_tn(TNP[i].c_str(), 0);
+		unsigned char (int)key = get_tn(TNP[i].c_str(), 0);
         std::cout << key <<" ";
         TNPmap[key] = 1;
 	}
