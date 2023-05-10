024a8fbf799548a06cde622f34b61a5f23d9ea9e
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-09 21:35:44 -0400

    Update TNF2.cu

--- cuda/TNF2.cu
+++ cuda/TNF2.cu
@@ -289,8 +289,9 @@ int main(int argc, char const *argv[]){
         cudaMemcpy(smallCtgs_d, smallCtgs_kernel, n_BLOCKS * n_THREADS, cudaMemcpyHostToDevice);
 
         get_TNF<<<grdDim, blkDim>>>(TNF_d, seqs_d, seqs_d_index, cont, TNmap_d, TNPmap_d, smallCtgs_d, gCtgIdx_d, 1);
-
         cudaDeviceSynchronize();
+
+        TNF.emplace_back((double *) malloc(n_BLOCKS * n_THREADS * n_TNF * sizeof(double)));
         cudaMemcpy(TNF[TNF.size() - 1], TNF_d, nobs * n_TNF * sizeof(double), cudaMemcpyDeviceToHost);
 
         std::cout << "kernel: " << kernel_cont<< std::endl;
