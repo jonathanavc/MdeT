c11df50ccbd64029ac6abcf21704b68ce4e42aa2
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-11 13:14:26 -0400

    a

--- comp.sh
+++
@@ -1,5 +0,0 @@
-g++ ./openmp/TNF.cpp -lz -fopenmp -O3 -o omp
-nvcc ./cuda/TNF.cu -lz -O3 -o cuda
-nvcc ./cuda/TNF2.cu -lz -O3 -o cuda2
-g++ ./extra/comp.cpp -lz -fopenmp -O3 -o comp
-
---
+++ compilar.sh
@@ -0,0 +1,5 @@
+g++ ./openmp/TNF.cpp -lz -fopenmp -O3 -o omp_ex
+nvcc ./cuda/TNF.cu -lz -O3 -o cuda_ex
+nvcc ./cuda/TNF2.cu -lz -O3 -o cuda2_ex
+g++ ./extra/comp.cpp -lz -fopenmp -O3 -o comp_ex
+
