ce1b9308dd40b8bb251315d64ab947dbd5db8358
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 16:24:18 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ tiempos = {}
 for thead in theads:
     for i in range(1, num_ex):
         print("OMP "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-        p = subprocess.call(['time', './omp_ex', str(thead)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        out = p.communicate()
+        p = subprocess.Popen(['time', './omp_ex', str(thead)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
         tiempos["omp"][thead][i] = out
         print(tiempos)
 
@@ -20,16 +20,16 @@ for bloq in cuda_bloqs:
     for thead in cuda_threads:
         for i in range(1, num_ex):
             print("["+str(thead)+"/"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-            p = subprocess.call(['time','./cuda_ex', str(bloq), str(theads)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-            out = p.communicate()
+            p = subprocess.Popen(['time','./cuda_ex', str(bloq), str(theads)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
             tiempos["cuda"][bloq][thead][i] = out
-            
+
 for bloq in cuda_bloqs:
     for thead in cuda_threads: 
         for i in range(1, num_ex):
             print("["+str(thead)+"/"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-            p = subprocess.call(['time','./cuda2_ex', str(bloq), str(theads)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-            out = p.communicate()
+            p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(theads)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
             tiempos["cuda2"][bloq][thead][i] = out
 
 _json = json.dumps(tiempos)
