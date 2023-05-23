b09ba26a29c80f048cd1802cef3bf3433a85de38
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 16:51:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -18,9 +18,9 @@ for thread in threads:
     tiempos['omp']['n_threads'][str(thread)] = []
     for i in range(1, num_ex):
         print("OMP "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-        p = subprocess.Popen(['time', './omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        out, err = p.communicate()
-        tiempos['omp']['n_threads'][str(thread)].append(err)
+        p = subprocess.Popen(['time', './omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=False)
+        err, out = p.communicate()
+        tiempos['omp']['n_threads'][str(thread)].append(out)
         print(tiempos)
 
 tiempos['cuda'] = {
@@ -37,8 +37,8 @@ for bloq in cuda_bloqs:
         for i in range(1, num_ex):
             print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['time','./cuda_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = p.communicate()
-            tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(err)
+            err, out = p.communicate()
+            tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
 
 tiempos['cuda2'] = {
     'n_bloq':{
@@ -54,8 +54,8 @@ for bloq in cuda_bloqs:
         for i in range(1, num_ex):
             print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = p.communicate()
-            tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(err)
+            err, out = p.communicate()
+            tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
 
 _json = json.dumps(tiempos)
 
