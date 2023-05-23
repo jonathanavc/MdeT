55bded5d96136ef32ad6dc221fbf51f8d1f157cc
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 17:39:56 -0400

    Update test.py

--- test.py
+++ test.py
@@ -22,7 +22,6 @@ for thread in threads:
         p = subprocess.Popen(['./omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         err, out = p.communicate()
         print(out)
-        print(re.finditer())
         tiempos['omp']['n_threads'][str(thread)].append(re.findall(r"[-+]?(?:\d*\.*\d+)", out))
         print(tiempos)
 """
