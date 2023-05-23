87cefb233e014f1abdc94ca82aedce25a0a29c91
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 16:52:36 -0400

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ for thread in threads:
     tiempos['omp']['n_threads'][str(thread)] = []
     for i in range(1, num_ex):
         print("OMP "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-        p = subprocess.Popen(['time', './omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(['time', './omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         err, out = p.communicate()
         tiempos['omp']['n_threads'][str(thread)].append(out)
         print(tiempos)
@@ -36,7 +36,7 @@ for bloq in cuda_bloqs:
         tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)] = []
         for i in range(1, num_ex):
             print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-            p = subprocess.Popen(['time','./cuda_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p = subprocess.Popen(['time','./cuda_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             err, out = p.communicate()
             tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
 
@@ -53,7 +53,7 @@ for bloq in cuda_bloqs:
         tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)] = []
         for i in range(1, num_ex):
             print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-            p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             err, out = p.communicate()
             tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
 
