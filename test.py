648c56877ca6eb9f3e57617f4275b76cbcb1cf6c
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 19:43:26 -0400

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ cuda_bloqs = [32,64,128,256,512,1024,2048]
 
 tiempos = {}
 
-"""
+#OMP
 tiempos['omp'] = {
     'n_threads':{
     }
@@ -25,24 +25,25 @@ for thread in threads:
         out, err = p.communicate()
         tiempos['omp']['n_threads'][str(thread)].append(re.findall(r"[-+]?(?:\d*\.*\d+)", out))
 
+#CUDA
 tiempos['cuda'] = {
-    'n_bloq':{
+    'n_bloqs':{
     }
 }
 for bloq in cuda_bloqs:
-    tiempos['cuda']['n_bloq'][str(bloq)] = {
+    tiempos['cuda']['n_bloqs'][str(bloq)] = {
             'n_threads':{
         }
     }
-    for thread in cuda_threads:
-        tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)] = []
+    for thread in cuda_threads: 
+        tiempos['cuda']['n_bloqs'][str(bloq)]['n_threads'][str(thread)] = []
         for i in range(1, num_ex):
-            print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
+            print("[T:"+str(thread)+"/B:"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['./cuda_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             out, err = p.communicate()
-            tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
-"""
+            tiempos['cuda']['n_bloqs'][str(bloq)]['n_threads'][str(thread)].append(out)
 
+#CUDA V2
 tiempos['cuda2'] = {
     'n_bloqs':{
     }
@@ -55,12 +56,12 @@ for bloq in cuda_bloqs:
     for thread in cuda_threads: 
         tiempos['cuda2']['n_bloqs'][str(bloq)]['n_threads'][str(thread)] = []
         for i in range(1, num_ex):
-            print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
+            print("[T:"+str(thread)+"/B:"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             out, err = p.communicate()
             tiempos['cuda2']['n_bloqs'][str(bloq)]['n_threads'][str(thread)].append(out)
 
+#GUARDAR
 _json = json.dumps(tiempos)
-
 with open(datetime.now().strftime("Test_%d/.%m.%Y_H.%M.%S")+".json", "w") as outfile:
     outfile.write(_json)
