75185d689279c803e52da9e5e534fba9572a69a2
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 16:53:55 -0400

    Update test.py

--- test.py
+++ test.py
@@ -21,8 +21,7 @@ for thread in threads:
         p = subprocess.Popen(['time', './omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         err, out = p.communicate()
         tiempos['omp']['n_threads'][str(thread)].append(out)
-        print(tiempos)
-
+"""
 tiempos['cuda'] = {
     'n_bloq':{
     }
@@ -56,7 +55,7 @@ for bloq in cuda_bloqs:
             p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             err, out = p.communicate()
             tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
-
+"""
 _json = json.dumps(tiempos)
 
 with open("sample.json", "w") as outfile:
