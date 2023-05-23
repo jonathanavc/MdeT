f957dea6bc5ec0c38cb475c62f28685a3f8aac5a
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 19:58:04 -0400

    Update test.py

--- test.py
+++ test.py
@@ -39,8 +39,8 @@ for bloq in cuda_bloqs:
     }
     for thread in cuda_threads: 
         tiempos['cuda']['n_bloqs'][str(bloq)]['n_threads'][str(thread)] = []
-        for i in range(1, num_ex):
-            print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda "+ str((i/num_ex) * 100) + "%", end='\r')
+        for i in range(0, num_ex):
+            print("[T:"+str(thread)+"/B:"+str(bloq)+"]"+"Cuda "+ str((i/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['./cuda_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             out, err = p.communicate()
             tiempos['cuda']['n_bloqs'][str(bloq)]['n_threads'][str(thread)].append(out)
@@ -57,7 +57,7 @@ for bloq in cuda_bloqs:
     }
     for thread in cuda_threads: 
         tiempos['cuda2']['n_bloqs'][str(bloq)]['n_threads'][str(thread)] = []
-        for i in range(1, num_ex):
+        for i in range(0, num_ex):
             print("[T:"+str(thread)+"/B:"+str(bloq)+"]"+"Cuda2 "+ str((i/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             out, err = p.communicate()
