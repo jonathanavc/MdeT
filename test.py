cdb59990c7e5c642f7d9d699652feaac4991dbb3
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-23 16:41:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,31 +6,56 @@ theads = [1,2,4,6,8,10,10]
 cuda_threads = [16,32,64,128]
 cuda_bloqs = [32,64,128,256,512,1024,2048]
 
+
+
 tiempos = {}
 
-for thead in theads:
+tiempos['omp'] = {
+    'n_threads':{
+    }
+}
+for thread in threads:
+    tiempos['omp']['n_threads'][str(thread)] = []
     for i in range(1, num_ex):
         print("OMP "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-        p = subprocess.Popen(['time', './omp_ex', str(thead)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(['time', './omp_ex', str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
-        tiempos["omp"][thead][i] = out
+        tiempos['omp']['n_threads'][str(thread)].append(out)
         print(tiempos)
 
+tiempos['cuda'] = {
+    'n_bloq':{
+    }
+}
 for bloq in cuda_bloqs:
-    for thead in cuda_threads:
+    tiempos['cuda']['n_bloq'][str(bloq)] = {
+            'n_threads':{
+        }
+    }
+    for thread in cuda_threads:
+        tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)] = []
         for i in range(1, num_ex):
-            print("["+str(thead)+"/"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
+            print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
             p = subprocess.Popen(['time','./cuda_ex', str(bloq), str(theads)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
-            tiempos["cuda"][bloq][thead][i] = out
+            tiempos['cuda']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
 
+tiempos['cuda2'] = {
+    'n_bloq':{
+    }
+}
 for bloq in cuda_bloqs:
-    for thead in cuda_threads: 
+    tiempos['cuda2']['n_bloq'][str(bloq)] = {
+            'n_threads':{
+        }
+    }
+    for thread in cuda_threads: 
+        tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)] = []
         for i in range(1, num_ex):
-            print("["+str(thead)+"/"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
-            p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(theads)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print("["+str(thread)+"/"+str(bloq)+"]"+"Cuda2 "+ str(((i-1)/num_ex) * 100) + "%", end='\r')
+            p = subprocess.Popen(['time','./cuda2_ex', str(bloq), str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
-            tiempos["cuda2"][bloq][thead][i] = out
+            tiempos['cuda2']['n_bloq'][str(bloq)]['n_thread'][str(thread)].append(out)
 
 _json = json.dumps(tiempos)
 
