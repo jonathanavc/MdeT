03631f5adc11888a31ea0b0a030a21801d94260a
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-05-29 12:29:08 -0400

    Update TNF2.cu

--- cuda/TNF2.cu
+++ cuda/TNF2.cu
@@ -12,6 +12,10 @@
 
 __device__ __constant__ int n_TNF_d = 136;
 
+__device__ __constant__ unsigned char TNmap_d[256] = {2, 21, 31, 115, 101, 119, 67, 50, 135, 126, 69, 92, 116, 88, 8, 78, 47, 96, 3, 70, 106, 38, 48, 83, 16, 22, 136, 114, 5, 54, 107, 120, 72, 41, 44, 26, 27, 23, 136, 53, 12, 81, 136, 127, 30, 110, 136, 80, 132, 123, 71, 102, 79, 1, 35, 124, 29, 4, 136, 34, 91, 17, 136, 52, 9, 77, 136, 117, 76, 93, 136, 65, 6, 73, 136, 68, 28, 94, 136, 113, 121, 36, 136, 10, 103, 99, 136, 87, 129, 14, 136, 136, 98, 19, 136, 97, 15, 56, 136, 131, 57, 46, 136, 136, 122, 60, 136, 136, 42, 62, 136, 136, 7, 130, 136, 51, 133, 20, 136, 134, 89, 86, 136, 136, 104, 95, 136, 136, 49, 136, 136, 136, 105, 136, 136, 136, 33, 136, 136, 136, 43, 136, 136, 136, 55, 136, 136, 136, 112, 136, 136, 136, 136, 136, 136, 136, 75, 136, 136, 136, 32, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 100, 136, 136, 136, 63, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 136, 125, 108, 136, 136, 58, 24, 136, 136, 84, 13, 136, 136, 25, 66, 136, 136, 18, 128, 136, 136, 74, 61, 136, 136, 85, 136, 136, 136, 118, 40, 136, 136, 109, 90, 136, 136, 45, 136, 136, 136, 111, 136, 136, 136, 82, 136, 136, 136, 59, 11, 136, 136, 64, 37, 136, 136, 0, 136, 136, 136, 39, 136, 136, 136};
+__device__ __constant__ unsigned char TNPmap_d[256] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
 __device__ const char * get_contig_d(int contig_index, const char * seqs_d,const size_t * seqs_d_index){
     size_t contig_beg = 0;
     if(contig_index != 0){
@@ -59,54 +63,66 @@ __device__ unsigned char get_revComp_tn_d(const char * contig, size_t index){
     return tn;
 }
 
-__global__ void get_TNF(double * TNF_d , const char * seqs_d, const size_t * seqs_d_index , size_t nobs,
-    const unsigned char * TNmap, const unsigned char * TNPmap, const unsigned char * smallCtgs, size_t contigs_per_thread){
+__global__ void get_TNF(double *TNF_d, const char *seqs_d, const size_t *seqs_d_index, size_t nobs,
+                        const unsigned char *smallCtgs, const size_t *gCtgIdx_d, size_t contigs_per_thread)
+{
+
     size_t thead_id = threadIdx.x + blockIdx.x * blockDim.x;
+    // crea un tnf de forma local
+    double TNF_temp[136];
 
-    // inicializar valores de vector en 0
-    for(size_t i = 0; i < contigs_per_thread; i++){ 
-        size_t contig_index = (thead_id * contigs_per_thread) + i;
-        if(contig_index >= nobs) break;
-        for(int j = 0; j < n_TNF_d; j++){
-            TNF_d[contig_index * n_TNF_d + j] = 0;
+    for (size_t i = 0; i < contigs_per_thread; i++)
+    {
+        for (int j = 0; j < n_TNF_d; j++)
+        {
+            TNF_temp[j] = 0;
         }
-    }
-
-    //__syncthreads(); 
-
-    for(size_t i = 0; i < contigs_per_thread; i++){
         size_t contig_index = (thead_id * contigs_per_thread) + i;
-        if(contig_index >= nobs) break;
-        if(smallCtgs[contig_index] == 0){
-            const char * contig = get_contig_d(contig_index, seqs_d, seqs_d_index);
-            size_t contig_size = seqs_d_index[contig_index];
-            if(contig_index != 0){
-                contig_size -= seqs_d_index[contig_index - 1];
+        if (contig_index >= nobs)
+            break;
+        if (smallCtgs[contig_index] == 0)
+        {
+            const char *contig = get_contig_d(gCtgIdx_d[contig_index], seqs_d, seqs_d_index);
+            size_t contig_size = seqs_d_index[gCtgIdx_d[contig_index]];
+            if (gCtgIdx_d[contig_index] != 0)
+            {
+                contig_size -= seqs_d_index[gCtgIdx_d[contig_index] - 1];
             }
-            for (size_t j = 0; j < contig_size - 3; ++j) {
+            for (size_t j = 0; j < contig_size - 3; ++j)
+            {
                 unsigned char tn = get_tn(contig, j);
-                //SI tn NO SE ENCUENTRA EN TNmap el complemento del palindromo sí estará
-                if(TNmap[tn] != n_TNF_d){
-                    ++TNF_d[contig_index * n_TNF_d + TNmap[tn]];
+                // SI tn NO SE ENCUENTRA EN TNmap el complemento del palindromo sí estará
+                if (TNmap_d[tn] != n_TNF_d)
+                {
+                    ++TNF_temp[TNmap_d[tn]];
                 }
-                
+
                 tn = get_revComp_tn_d(contig, j);
 
-                //SALTA EL PALINDROMO PARA NO INSERTARLO NUEVAMENTE
-                if (TNPmap[tn] == 0) {
-                    if(TNmap[tn] != n_TNF_d){
-                        ++TNF_d[contig_index * n_TNF_d + TNmap[tn]];
+                // SALTA EL PALINDROMO PARA NO INSERTARLO NUEVAMENTE
+                if (TNPmap_d[tn] == 0)
+                {
+                    if (TNmap_d[tn] != n_TNF_d)
+                    {
+                        ++TNF_temp[TNmap_d[tn]];
                     }
                 }
             }
             double rsum = 0;
-            for(size_t c = 0; c < n_TNF_d; ++c) {
-                rsum += TNF_d[contig_index * n_TNF_d + c] * TNF_d[contig_index * n_TNF_d + c];
+            for (size_t c = 0; c < n_TNF_d; ++c)
+            {
+                rsum += TNF_temp[c] * TNF_temp[c];
             }
             rsum = sqrt(rsum);
-            for(size_t c = 0; c < n_TNF_d; ++c) {
-                TNF_d[contig_index * n_TNF_d + c] /= rsum; //OK
+            for (size_t c = 0; c < n_TNF_d; ++c)
+            {
+                TNF_temp[c] /= rsum; // OK
             }
+        }
+        // guardar en la memoria global
+        for (size_t c = 0; c < n_TNF_d; ++c)
+        {
+            TNF_d[contig_index * n_TNF_d + c] = TNF_temp[c];
         }
     }
 }
@@ -146,8 +162,6 @@ static size_t minContigByCorrForGraph = 1000; //for graph generation purpose
 double * TNF_d;
 static char * seqs_d;
 static size_t * seqs_d_index;
-static unsigned char * TNmap_d;
-static unsigned char * TNPmap_d;
 static unsigned char * smallCtgs_d;
 static size_t * gCtgIdx_d;
 
@@ -166,7 +180,7 @@ void kernel(dim3 blkDim, dim3 grdDim){
     cudaMemcpy(seqs_d_index, seqs_kernel_index, n_BLOCKS * n_THREADS  * sizeof(size_t), cudaMemcpyHostToDevice);            // seqs_index
     cudaMemcpy(smallCtgs_d, smallCtgs_kernel, n_BLOCKS * n_THREADS, cudaMemcpyHostToDevice);
 
-    get_TNF<<<grdDim, blkDim>>>(TNF_d, seqs_d, seqs_d_index, nobs_cont, TNmap_d, TNPmap_d, smallCtgs_d, 1);
+    get_TNF<<<grdDim, blkDim>>>(TNF_d, seqs_d, seqs_d_index, nobs_cont, smallCtgs_d, 1);
     seqs_kernel = "";
     kernel_cont++;
     nobs_cont = 0;
@@ -204,15 +218,7 @@ int main(int argc, char const *argv[]){
 
     auto start_global = std::chrono::system_clock::now();
     auto start = std::chrono::system_clock::now();
-
-    //TNmap
-    cudaMalloc(&TNmap_d, 256);
-    cudaMemcpy(TNmap_d, TNmap, 256, cudaMemcpyHostToDevice);
-
-    //TNPmap
-    cudaMalloc(&TNPmap_d, 256);
-    cudaMemcpy(TNPmap_d, TNPmap, 256, cudaMemcpyHostToDevice);   
-
+    
     dim3 blkDim (n_THREADS, 1, 1);
     dim3 grdDim (n_BLOCKS, 1, 1);
 
