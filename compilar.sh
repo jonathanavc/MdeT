79dbfb6f63a8f1a36356d79716fed9c9c95771aa
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-06 16:28:20 -0400

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,4 @@
 g++ ./openmp/TNF.cpp -lz -fopenmp -O3 -o omp_ex
 nvcc ./cuda/TNF.cu -lz -O3 -o cuda_ex
-nvcc ./cuda/TNF2.cu -lz -O3 --threads -o cuda2_ex
+nvcc ./cuda/TNF2.cu -lz -O3 -Xcompiler="-pthread" -o cuda2_ex
 g++ ./extra/comp.cpp -lz -fopenmp -O3 -o comp_ex
