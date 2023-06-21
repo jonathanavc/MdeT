849a239aae28a26e8d76edd01789f14632192c09
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-21 02:46:18 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -267,7 +267,7 @@ void kernel(dim3 blkDim, dim3 grdDim, int SUBP_IND, int cont, int size)
     cudaMemcpyAsync(seqs_d_index[SUBP_IND], seqs_kernel_index[SUBP_IND],
                     n_BLOCKS * n_THREADS * contig_per_thread * sizeof(size_t), cudaMemcpyHostToDevice,
                     _s[SUBP_IND]); // seqs_index
-    get_TNF_local<<<grdDim, blkDim, 0, _s[SUBP_IND]>>>(TNF_d[SUBP_IND], seqs_d, seqs_d_index[SUBP_IND], size,
+    get_TNF<<<grdDim, blkDim, 0, _s[SUBP_IND]>>>(TNF_d[SUBP_IND], seqs_d, seqs_d_index[SUBP_IND], size,
                                                        contig_per_thread);
     cudaFreeAsync(seqs_d, _s[SUBP_IND]);
     cudaMemcpyAsync(TNF[cont], TNF_d[SUBP_IND], n_BLOCKS * n_THREADS * contig_per_thread * n_TNF * sizeof(double),
