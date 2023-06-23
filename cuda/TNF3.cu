1f6121aa2ddd9e59815e71e0fc65c48e5ec2cb11
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-23 12:10:44 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -315,8 +315,8 @@ int main(int argc, char const *argv[]) {
               ++nresv;
             }
           }
-          seqs_kernel->append(kseq->seq.s);
-          //seqs_kernel[SUBP_IND] += kseq->seq.s;
+          //más eficiente que seqs_kernel[SUBP_IND] += kseq->seq.s
+          seqs_kernel[SUBP_IND]->append(kseq->seq.s);
           seqs_kernel_index[SUBP_IND][nobs_cont] = seqs_kernel[SUBP_IND].size();
           nobs++;
           nobs_cont++;
