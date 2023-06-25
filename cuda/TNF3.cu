0c23165bc75a69c499405ba9a50c0c4a57356845
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-24 20:16:36 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -302,7 +302,9 @@ int main(int argc, char const *argv[]) {
     cerr << "[Error!] can't open the sequence fasta file " << inFile << endl;
     return 1;
   } else {
-    
+
+    // tambien lento en secuencial 26s
+    /*
     auto _start = std::chrono::system_clock::now();
 
     std::ifstream file(inFile.c_str(), std::ios::binary);
@@ -313,7 +315,7 @@ int main(int argc, char const *argv[]) {
     auto _end = std::chrono::system_clock::now();
     std::chrono::duration<float, std::milli> _duration = _end - _start;
     std::cout <<"cargar archivo descomprimidO"<< _duration.count() / 1000.f << std::endl;
-
+    */
 
     // no funciona bien cargar comprimido y descomprimir en memoria
     /*
