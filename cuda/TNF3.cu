7417ae92490aa09850c9ab51e9093588a443df1f
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-26 18:20:01 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -365,7 +365,7 @@ int main(int argc, char const *argv[]) {
     }
     std::cout << "tam strings:" << contigs.size() << std::endl;
 
-    std::cout << contigs.at(contigs.size() - 1) << std::endl;
+    //std::cout << contigs.at(contigs.size() - 1) << std::endl;
 
     close(fpint);
 
