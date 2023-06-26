55f15759cdbec46c61e3fe5e9941b1407536f2c1
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-26 17:44:28 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -250,7 +250,7 @@ void kernel(dim3 blkDim, dim3 grdDim, int SUBP_IND, int cont, int size) {
 }
 
 void reader(int fpint, int id, size_t chunk, size_t _size, char *_mem) {
-  int readSz = pread(fpint, _mem, _size, id * chunk);
+  size_t readSz = pread(fpint, _mem, _size, id * chunk);
   if (readSz < _size) {
     cout << " error en lectura readSz " << readSz << " chunk " << _size << endl;
   }
@@ -315,7 +315,7 @@ int main(int argc, char const *argv[]) {
   } else {
     auto _start = std::chrono::system_clock::now();
 
-    int nth = 2;
+    int nth = 1;
     int fpint = -1;
 
     FILE *fp = fopen(inFile.c_str(), "r");
