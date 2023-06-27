296619ed35c3f33f6f57b3a1ed1fb532a4349051
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-27 15:50:16 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -388,7 +388,9 @@ int main(int argc, char const *argv[]) {
             TNF.push_back((double *)0);
             SUBPS[SUBP_IND] = std::thread(kernel, blkDim, grdDim, SUBP_IND, kernel_cont, nobs_cont);
         }
+        std::cout << seqs.size() << std::cout;
         seqs.shrink_to_fit();
+        std::cout << seqs.size() << std::cout;
         contig_names.shrink_to_fit();
 
         for (int i = 0; i < 2; i++) {
