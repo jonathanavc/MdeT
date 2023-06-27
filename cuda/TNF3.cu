6c765effa3bf3a0eab4c742828eb87726ada3eba
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-27 02:55:53 -0400

    Update TNF3.cu

--- cuda/TNF3.cu
+++ cuda/TNF3.cu
@@ -240,7 +240,7 @@ void reader(int fpint, int id, size_t chunk, size_t _size, char *_mem) {
   while (readSz < _size) {
     size_t _bytesres = _size - readSz;
     readSz += pread(fpint, _mem + (id * chunk) + readSz, _bytesres,
-                    (id * chunk) + readSz)
+                    (id * chunk) + readSz);
   }
 }
 
