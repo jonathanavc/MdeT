85a1d1c7cb5a160a3ef2773decae63b8a4a2d203
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-10 15:54:53 -0400

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -2,5 +2,5 @@ g++ ./openmp/TNF.cpp -lz -fopenmp -O3 -o omp_ex
 nvcc ./cuda/TNF.cu -lz -O3 -arch=native -o cuda_ex
 nvcc ./cuda/TNF2.cu -lz -O3 -arch=native -o cuda2_ex
 nvcc ./cuda/TNF3.cu -lz -std=c++17 -O3 -arch=native -Xcompiler="-pthread" -o cuda3_ex
-nvcc ./cuda/Cuda.cu -lz -std=c++17 -O3 -arch=native -Xcompiler="-pthread" -o cuda_final
+nvcc ./cuda/Cuda.cu -lz -std=c++17 -O3 -lboost_program_options -arch=native -Xcompiler="-pthread" -o cuda_final
 g++ ./extra/comp.cpp -lz -fopenmp -O3 -o comp_ex
