19112a07a11957bf35535b133a78ca298356f556
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-11 18:21:13 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -621,9 +621,9 @@ int main(int argc, char const *argv[]) {
             if (i == n_STREAMS - 1) contig_to_procces += (nobs % n_STREAMS);
             cudaStreamCreate(&streams[i]);
             char *_mem_i = _mem + seqs_h_index_i[contig_per_kernel * i];  // puntero al inicio del primer contig
-            char *_mem_e = _mem + seqs_h_index_e[contig_per_kernel * i + contig_to_proccess] - 1; // puntero al final del ultimo contig
+            char *_mem_e = _mem + seqs_h_index_e[contig_per_kernel * i + contig_to_proccess]; // puntero al final del ultimo contig
             cudaMemcpyAsync(seqs_d, _mem_i, _mem_e - _mem_i, cudaMemcpyHostToDevice, streams[i]);
-            size_t contigs_per_thread = (nobs + (n_THREADS * n_BLOCKS) - 1) / (n_THREADS * n_BLOCKS);
+            size_t contigs_per_thread = (contig_to_procces + (n_THREADS * n_BLOCKS) - 1) / (n_THREADS * n_BLOCKS);
             get_TNF<<<grdDim, blkDim, 0, streams[i]>>>(TNF_d, seqs_d, seqs_d_index, contig_to_procces, contigs_per_thread, nobs);
             cudaMemcpyAsync(TNF, TNF_d + (contig_per_kernel * i * 136), contig_to_procces * 136 * sizeof(double),
                             cudaMemcpyDeviceToHost, streams[i]);
