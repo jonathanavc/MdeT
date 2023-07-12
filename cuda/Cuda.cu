d16c959eed116bff87214c0dfe919201781d700b
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-11 20:08:05 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -620,16 +620,14 @@ int main(int argc, char const *argv[]) {
         cudaMemcpy(seqs_d_index + nobs, seqs_h_index_e.data(), nobs * sizeof(size_t), cudaMemcpyHostToDevice);
 
         for (int i = 0; i < n_STREAMS; i++) {
+            cudaStreamCreate(&streams[i]);
             size_t _des = contig_per_kernel * i;
-            std::cout << "stream: " << i << std::endl;
+            std::cout << "stream: " << i < < < < " contig_to_process: " << contig_to_process << std::endl;
             size_t contig_to_process = contig_per_kernel;
             if (i == n_STREAMS - 1) contig_to_process += (nobs % n_STREAMS);
-            std::cout << "contig_to_process: " << contig_to_process << std::endl;
-            cudaStreamCreate(&streams[i]);
-            size_t _mem_i = seqs_h_index_i[_des];  // puntero al inicio del primer contig
-            std::cout << "_mem_i: " << _mem_i << std::endl;
-            size_t _mem_size = seqs_h_index_e[_des + contig_to_process - 1] - seqs_h_index_i[_des];
-            std::cout << "_mem_size: " << _mem_size << std::endl;
+            size_t _mem_i = seqs_h_index_i[_des];  // puntero al inicio del primer contig a procesar
+            size_t _mem_size =
+                seqs_h_index_e[_des + contig_to_process - 1] - seqs_h_index_i[_des];  // tamaño de la memoria a copiar
             size_t TNF_i = _des * 136;
             size_t contigs_per_thread = (contig_to_process + (n_THREADS * n_BLOCKS) - 1) / (n_THREADS * n_BLOCKS);
             cudaMemcpyAsync(seqs_d + _mem_i, _mem + _mem_i, _mem_size, cudaMemcpyHostToDevice, streams[i]);
