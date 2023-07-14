79acfcab4a223a2a7b81079aa34f00c867f684a0
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-14 18:39:59 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -696,7 +696,7 @@ int main(int argc, char const *argv[]) {
         contig_names.shrink_to_fit();    // liberar memoria no usada
         // TIMERSTOP(read_file);
     }
-    std::cout << contig_names[0] << std::endl;
+    //std::cout << contig_names[0] << std::endl;
 
     nobs2 = ignored.size();
 
