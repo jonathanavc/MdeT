e823eb3cff8f0b3e1701dec1de60ba465528bd65
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-15 02:51:28 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -1052,6 +1052,7 @@ int main(int argc, char const *argv[]) {
                 }
             }
         }
+        out.write((char *)&minContig, sizeof(size_t));
         out.write((char *)TNF, nobs * 136 * sizeof(double));
         out.close();
     } else {
