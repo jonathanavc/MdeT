f190b1b9363a2cb80fccebfb363c9f9098df9c7b
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-19 16:38:43 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -1226,7 +1226,7 @@ int main(int argc, char const *argv[]) {
     }
     std::cout << "... ";
     for (size_t i = ((nobs * (nobs - 1)) / 2) - 10; i < (nobs * (nobs - 1)) / 2; i++) {
-        std::cout << gprob[i] << " ";
+        std::cout << (int)gprob[i] << " ";
     }
     std::cout << std::endl;
 
