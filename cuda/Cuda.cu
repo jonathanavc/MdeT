d01de1435cd2b4485fb591fb1e1f626b744e435f
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-19 01:45:50 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -138,7 +138,7 @@ __device__ void cal_graph(double *gprob, double *TNF, double *ABD, size_t offset
     const size_t thead_id = threadIdx.x + blockIdx.x * blockDim.x;
     for (size_t i = 0; i < contig_per_thread; i++) {
         size_t prob_id = offset + (thead_id * contig_per_thread) + i;
-        size_t r1 = 0.5 * (sqrt(8 * prob_id + 1) + 1);
+        size_t r1 = 0.5 * (sqrt(double(8 * prob_id + 1)) + 1);
         size_t r2 = prob_id - (r1 * (r1 - 1) / 2);
         gprob[prob_id] = 1. - cal_dist(r1, r2, ABD, TNF, seqs_d_index, seqs_d_index_size);
     }
