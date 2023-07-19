68f9720e19f3d9d2a39c49eebc3522f74ad5bd1d
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-19 16:10:12 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -176,7 +176,7 @@ __global__ void get_prob(double *gprob_d, double *TNF_d, double *ABD_d, size_t o
         if (gprob_index >= limit) break;
         r1 = (sqrtf(1 + 8 * gprob_index) - 1) / 2;
         r2 = gprob_index - r1 * (r1 + 1);
-        gprob_d[gprob_index] = r1;
+        gprob_d[gprob_index] = r2;
     }
 }
 
