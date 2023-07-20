b1e38c76ec22bd3565f83356c09024f589ea2ab3
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-19 20:51:53 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -492,7 +492,7 @@ Distance cal_tnf_dist(size_t r1, size_t r2) {
     Distance d = 0;
 
     for (size_t i = 0; i < 136; ++i) {
-        d += (TNF[r1 * 136 + i] - TNF[r2 * 135 + i]) * (TNF[r1 * 136 + i] - TNF[r2 * 136 + i]);  // euclidean distance
+        d += (TNF[r1 * 136 + i] - TNF[r2 * 136 + i]) * (TNF[r1 * 136 + i] - TNF[r2 * 136 + i]);  // euclidean distance
     }
 
     d = sqrt(d);
