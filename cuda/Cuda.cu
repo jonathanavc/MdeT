b02a4a6c5ee2c786e7ec0c0cde4c607c53345770
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-29 14:28:46 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -51,7 +51,7 @@
 namespace po = boost::program_options;
 
 // texture<float, cudaTextureType1D, cudaReadModeElementType> texTNF;
-texture<float, cudaTextureType1DLayered, cudaReadModeElementType> texRef;
+//texture<float, cudaTextureType1DLayered, cudaReadModeElementType> texRef;
 
 __device__ __constant__ unsigned char TNmap_d[256] = {
     2,   21,  31,  115, 101, 119, 67,  50,  135, 126, 69,  92,  116, 88,  8,   78,  47,  96,  3,   70,  106, 38,  48,  83,  16,  22,
