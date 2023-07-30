3a636ec8b0afe4c06469482af3ad3241a34a795f
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-07-30 14:35:18 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -2286,6 +2286,7 @@ int main(int argc, char const *argv[]) {
     if (!loadDistanceFromFile(saveDistanceFile, requiredMinP, minContig)) {
         ProgressTracker progress = ProgressTracker(nobs * (nobs - 1) / 2, nobs / 100 + 1);
         gprob.m_vertices.resize(nobs);
+        /*
 #pragma omp parallel for schedule(dynamic)
         for (size_t i = 0; i < nobs * (nobs - 1) / 2; i++) {
             long long discriminante = 1 + 8 * i;
@@ -2299,42 +2300,32 @@ int main(int argc, char const *argv[]) {
                     boost::add_edge(col, row, Weight(s), gprob);
                 }
             }
-            /*
-            if (verbose) {
-                progress.track(i);
-                if (omp_get_thread_num() == 0) {
-                    progress.setProgress(i);
-                    verbose_message("Building a probabilistic graph: %s\r", progress.getProgress());
-                }
-            }
-            */
         }
         verbose_message("Finished building a probabilistic graph. (%d vertices and %d edges)          \n", boost::num_vertices(gprob),
                         boost::num_edges(gprob));
+*/
 
-        /*
-        #pragma omp parallel for schedule(dynamic)
-                for (size_t i = 1; i < nobs; ++i) {
-                    if (smallCtgs.find(i) == smallCtgs.end()) {        // Don't build graph for small contigs
-                        for (size_t j = 0; j < i; ++j) {               // populate lower triangle
-                            if (smallCtgs.find(j) != smallCtgs.end())  // Don't build graph for small contigs
-                                continue;
-                            bool passed = false;
-                            // Similarity s = 1. - tnf_prob[((i * (i - 1)) / 2) + j];
-                            Similarity s = 1. - cal_dist(i, j, 1. - requiredMinP, passed);
-                            if (passed && s >= requiredMinP) {
-        #pragma omp critical(ADD_EDGE_1)
-                                { boost::add_edge(i, j, Weight(s), gprob); }
-                            }
-                        }
-                    }
-                    if (verbose) {
-                        progress.track(i);
-                        if (omp_get_thread_num() == 0 && progress.isStepMarker())
-                            verbose_message("Building a probabilistic graph: %s\r", progress.getProgress());
+#pragma omp parallel for schedule(dynamic)
+        for (size_t i = 1; i < nobs; ++i) {
+            if (smallCtgs.find(i) == smallCtgs.end()) {        // Don't build graph for small contigs
+                for (size_t j = 0; j < i; ++j) {               // populate lower triangle
+                    if (smallCtgs.find(j) != smallCtgs.end())  // Don't build graph for small contigs
+                        continue;
+                    bool passed = false;
+                    Similarity s = 1. - tnf_prob[((i * (i - 1)) / 2) + j];
+                    // Similarity s = 1. - cal_dist(i, j, 1. - requiredMinP, passed);
+                    if (passed && s >= requiredMinP) {
+#pragma omp critical(ADD_EDGE_1)
+                        { boost::add_edge(i, j, Weight(s), gprob); }
                     }
                 }
-                */
+            }
+            if (verbose) {
+                progress.track(i);
+                if (omp_get_thread_num() == 0 && progress.isStepMarker())
+                    verbose_message("Building a probabilistic graph: %s\r", progress.getProgress());
+            }
+        }
         // saveDistanceToFile(saveDistanceFile, requiredMinP, minContig);
     }
 
