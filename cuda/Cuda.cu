10a9618e39ab97b3ce3fa3da1e9739cf572ffff4
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-08-15 23:46:43 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -1820,7 +1820,7 @@ void launch_kernel(size_t cobs, size_t _first, size_t global_des) {
         size_t TNF_des = _des * 136;
         if (i == n_STREAMS - 1) contig_to_process += (cobs % n_STREAMS);
         size_t contigs_per_thread = (contig_to_process + (numThreads2 * numBlocks) - 1) / (numThreads2 * numBlocks);
-        cudaMemcpyAsync(seqs_d + seqs_h_index_i[_des], _mem + seqs_h_index_i[_des],
+        cudaMemcpyAsync(seqs_d + seqs_h_index_i[_des], &seqs[gCtgIdx[_first]][0] + seqs_h_index_i[_des],
                         seqs_h_index_e[_des + contig_to_process - 1] - seqs_h_index_i[_des], cudaMemcpyHostToDevice, streams[i]);
         cudaStreamSynchronize(streams[i]);
         std::cout << "cpy->device(seqs_d) " << seqs_h_index_i[_des] << " " << seqs_h_index_e[_des + contig_to_process - 1] << " "
