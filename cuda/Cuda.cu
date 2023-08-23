3434e59e76b9ffcb9e9175b6cc39ca7780381aaf
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-08-23 04:12:22 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -91,17 +91,17 @@ __device__ __constant__ double _c2[19] = { 39406.5712626297, -77863.1741143294, 
 
 __device__ double log10_device(double x) { return log(x) / _log10; }
 
-__device__ double cal_tnf_dist_d(size_t r1, size_t r2, float *__restrict__ TNF1, float *__restrict__ TNF2,size_t *__restrict__ seqs_d_size) {
+__device__ double cal_tnf_dist_d(size_t r1, size_t r2, float *__restrict__ TNF1, float *__restrict__ TNF2) {
     double d = 0.0;
     for (size_t i = 0; i < 136; ++i) {
         d += (TNF1[i] - TNF2[i]) * (TNF1[i] - TNF2[i]);  // euclidean distance
     }
     d = sqrt(d);
     double b, c;
-    size_t ctg1_s = seqs_d_size[r1];
-    size_t ctg2_s = seqs_d_size[r2];
-    size_t ctg1 = min(ctg1_s, (size_t)500000);
-    size_t ctg2 = min(ctg2_s, (size_t)500000);
+    //size_t ctg1_s = seqs_d_size[r1];
+    //size_t ctg2_s = seqs_d_size[r2];
+    size_t ctg1 = min(r1, (size_t)500000);
+    size_t ctg2 = min(r2, (size_t)500000);
     double lw[19];
     lw[0] = log10_device(min(ctg1, ctg2));
     lw[1] = log10_device(max(ctg1, ctg2));
@@ -234,16 +234,20 @@ __global__ void get_tnf_prob(double *__restrict__ tnf_dist, float *__restrict__ 
     size_t r2;
     size_t tnf_dist_index = (threadIdx.x + blockIdx.x * blockDim.x) * contig_per_thread;
     size_t prob_index = _des + tnf_dist_index;
-
     double discriminante = 1 + 8 * prob_index;
     r1 = (1 + sqrt(discriminante)) / 2;
     r2 = prob_index - r1 * (r1 - 1) / 2;
     size_t _limit = min(prob_index + contig_per_thread, limit);
     while(prob_index < _limit){
+        float _TNF[136];
         //if (prob_index == limit) break;
         while(r2 < r1){
+            for (int i = 0; i < 136; i++) {
+                _TNF[i] = TNF[r1 * 136 + i];
+            }
+
             if (prob_index == _limit) break;
-            tnf_dist[tnf_dist_index] = cal_tnf_dist_d(r1, r2, TNF + r1*136, TNF + r2*136, seqs_d_size);
+            tnf_dist[tnf_dist_index] = cal_tnf_dist_d(seqs_d_size[r1], seqs_d_size[r2], _TNF, TNF + r2 * 136);
             tnf_dist_index++;
             prob_index++;
             r2++;
