08fb78528b21518649d443498ce49fadd0c70ee7
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-08-26 20:42:27 -0400

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -253,13 +253,15 @@ __global__ void get_tnf_prob(double *__restrict__ tnf_dist, float *__restrict__ 
     r2 = prob_index - r1 * (r1 - 1) / 2;
     size_t _limit = min(prob_index + contig_per_thread, limit);
     while(prob_index < _limit){
+        /*
         float _TNF[136];
         for (int i = 0; i < 136; i++) {
             _TNF[i] = TNF[r1 * 136 + i];
         }
+        */
         while(r2 < r1){
             if (prob_index == _limit) break;
-            tnf_dist[tnf_dist_index] = r1;
+            tnf_dist[tnf_dist_index] = cal_tnf_dist_d(seqs_d_size[r1], seqs_d_size[r2], TNF + r1 * 136, TNF + r2 * 136);
             tnf_dist_index++;
             prob_index++;
             r2++;
