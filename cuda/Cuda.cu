2aaeac53e8fc85a09ea3c759df04e3bc86fca7a7
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-09-08 16:15:22 -0300

    Update Cuda.cu

--- cuda/Cuda.cu
+++ cuda/Cuda.cu
@@ -2390,7 +2390,7 @@ int main(int argc, char const *argv[]) {
 
         assert(rABD.size() == nobs);
     }
-    size_t max_gpu_mem = 1000000000;  // 4gb
+    size_t max_gpu_mem = 4000000000;  // 4gb
     // calcular matriz de tetranucleotidos
     contigs.reserve(nobs);
     for (int i = 0; i < nobs; i++) {
