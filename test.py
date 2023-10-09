6f6139dfeaabfc0d7e0df70e6554c6d6c43117d2
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-10-08 21:01:26 -0300

    Update test.py

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ def std(arr):
     avg = sum(arr)/len(arr)
     return sum([(x-avg)**2 for x in arr])/len(arr)
 
+'''
 print("Metabat1")
 for thread in threads:
     tiempos[thread] = {
@@ -30,18 +31,13 @@ for thread in threads:
         'binning': []
     }
     for i in range(0, num_ex):
-        #print("[T:"+str(thread)+']'+"metabat1 "+ str((i/num_ex) * 100) + "%", end='\r')
         p = subprocess.Popen(['./metabat1','-i' + archivo, '-o'+'out/out', '-t' + str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         out, err = p.communicate()
         valores = re.findall(r"[-+]?(?:\d*\.*\d+)", out)
-        #print(type(valores[0]))
-        #print(float(valores[0]))
         tiempos[thread]['read'].append(float(valores[0]))
         tiempos[thread]['tnf'].append(float(valores[1]))
         tiempos[thread]['prob'].append(float(valores[2]))
         tiempos[thread]['binning'].append(float(valores[4]))
-        #tiempos[thread] += [valores[0], valores[1], valores[3], valores[2]]
-        #print("[T:" + str(thread) + "] metabat1 " + str(((i + 1) / num_ex) * 100) + "%" + " read: " + str(avg(tiempos[thread]['read'])) + "±" + str(std(tiempos[thread]['read'])) + " tnf: " + str(avg(tiempos[thread]['tnf'])) + "±" + str(std(tiempos[thread]['tnf'])) + " prob: " + str(avg(tiempos[thread]['prob'])) + "±" + str(std(tiempos[thread]['prob'])) + " binning: " + str(avg(tiempos[thread]['binning'])) + "±" + str(std(tiempos[thread]['binning'])), end='\r')
         print("[T:{}] Metabat1 {:.0f}% Read: {:.4f}±{:.4f} Tnf: {:.4f}±{:.4f} Prob: {:.4f}±{:.4f} Binning: {:.4f}±{:.4f}".format(
             thread,
             ((i + 1) / num_ex) * 100,
@@ -67,7 +63,72 @@ for thread in threads:
         "prob": std(tiempos[thread]['prob']),
         "binning": std(tiempos[thread]['binning'])
     }
-    print()
+'''
+
+print("Metabat CUDA")
+for stream in cuda_streams:
+    tiempos[stream] = {}
+    tiempos[stream]["read"] = ""
+    tiempos[stream]["tnf"] = ""
+    tiempos[stream]["prob"] = ""
+    tiempos[stream]["binning"] = ""
+    for cthread in cuda_threads:
+        tiempos[stream]["read"] += str(cthread) + " "
+        tiempos[stream]["tnf"] += str(cthread) + " "
+        tiempos[stream]["prob"] += str(cthread) + " "
+        tiempos[stream]["binning"] += str(cthread) + " "
+        tiempos[stream][cthread] = {}
+        for cbloq in cuda_bloqs:
+            tiempos[stream]["read"] += str(cbloq) + " "
+            tiempos[stream]["tnf"] += str(cbloq) + " "
+            tiempos[stream]["prob"] += str(cbloq) + " "
+            tiempos[stream]["binning"] += str(cbloq) + " "
+            tiempos[stream][cthread][cbloq] = {
+                'read': [],
+                'tnf': [],
+                'prob': [],
+                'binning': []
+            }
+            for i in range(0, num_ex):
+                p = subprocess.Popen(['./metabat1','-i' + archivo, '-o'+'out/out', '-t' + str(thread)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                out, err = p.communicate()
+                valores = re.findall(r"[-+]?(?:\d*\.*\d+)", out)
+                tiempos[stream][cthread][cbloq]['read'].append(float(valores[0]))
+                tiempos[stream][cthread][cbloq]['tnf'].append(float(valores[1]))
+                tiempos[stream][cthread][cbloq]['prob'].append(float(valores[2]))
+                tiempos[stream][cthread][cbloq]['binning'].append(float(valores[4]))
+                print("[cs:{};ct:{};cb:{}] Metabat1 {:.0f}% Read: {:.4f}±{:.4f} Tnf: {:.4f}±{:.4f} Prob: {:.4f}±{:.4f} Binning: {:.4f}±{:.4f}".format(
+                    stream,cthread,cbloq,
+                    ((i + 1) / num_ex) * 100,
+                    avg(tiempos[stream][cthread][cbloq]['read']),
+                    std(tiempos[stream][cthread][cbloq]['read']),
+                    avg(tiempos[stream][cthread][cbloq]['tnf']),
+                    std(tiempos[stream][cthread][cbloq]['tnf']),
+                    avg(tiempos[stream][cthread][cbloq]['prob']),
+                    std(tiempos[stream][cthread][cbloq]['prob']),
+                    avg(tiempos[stream][cthread][cbloq]['binning']),
+                    std(tiempos[stream][cthread][cbloq]['binning'])
+                ), end='\r')
+            
+            tiempos[stream][cthread][cbloq]['avg'] = {
+                "read": avg(tiempos[stream][cthread][cbloq]['read']),
+                "tnf": avg(tiempos[stream][cthread][cbloq]['tnf']),
+                "prob": avg(tiempos[stream][cthread][cbloq]['prob']),
+                "binning": avg(tiempos[stream][cthread][cbloq]['binning'])
+            }
+            tiempos[stream][cthread][cbloq]['std'] = {
+                "read": std(tiempos[stream][cthread][cbloq]['read']),
+                "tnf": std(tiempos[stream][cthread][cbloq]['tnf']),
+                "prob": std(tiempos[stream][cthread][cbloq]['prob']),
+                "binning": std(tiempos[stream][cthread][cbloq]['binning'])
+            }
+            tiempos[stream]["read"] += str(tiempos[stream][cthread][cbloq]['avg']['read']) + "\n"
+            tiempos[stream]["tnf"] += str(tiempos[stream][cthread][cbloq]['avg']['tnf']) + "\n"
+            tiempos[stream]["prob"] += str(tiempos[stream][cthread][cbloq]['avg']['prob']) + "\n"
+            tiempos[stream]["binning"] += str(tiempos[stream][cthread][cbloq]['avg']['binning']) + "\n"
+   
+
+
 
 '''
 for bloq in cuda_bloqs:
