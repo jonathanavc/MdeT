702df734e16e54e96507b22429aaa1b9ac84b354
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-10-26 22:52:51 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -255,16 +255,14 @@ __device__ double cal_tnf_dist_d2(double r1, double r2, float* TNF1, float* TNF2
     const double floor_prob = 0.1;
     const double floor_preProb = log((1.0 / floor_prob) - 1.0);
 
-    double d = 0.0;
-    float tn1, tn2, _diff;
+    double d = 0.0, _diff;
+    float tn1, tn2;
     for (size_t i = 0; i < 136; ++i) {
-        d += (TNF1[i] - TNF2[i]) * (TNF1[i] - TNF2[i]);
-        /*
+        // d += (TNF1[i] - TNF2[i]) * (TNF1[i] - TNF2[i]);
         tn1 = TNF1[i];
         tn2 = TNF2[i];
         _diff = tn1 - tn2;
         d += _diff * _diff;
-        */
     }
 
     d = sqrt(d);
