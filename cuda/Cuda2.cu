f2b7321ba001defbf31f21e6466ed5806473865e
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-10-27 17:12:44 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -527,7 +527,7 @@ void launch_tnf_max_prob_sample_kernel(std::vector<size_t> idx, double* max_dist
         // get_tnf_max_prob_sample<<<blocks, threads>>>(max_dist_d, TNF_d, contig_log, contigs_d, nobs, _nobs, prob_per_thread);
         cudaMemcpyAsync(max_dist_h, max_dist_d, _nobs * sizeof(double), cudaMemcpyDeviceToHost);
     }
-    test(1) {
+    if (1) {
         double* _memtest;
         cudaMallocHost((void**)&_memtest, _nobs * sizeof(double));
         size_t prob_per_thread = (_nobs + (numThreads2 * numBlocks) - 1) / (numThreads2 * numBlocks);
