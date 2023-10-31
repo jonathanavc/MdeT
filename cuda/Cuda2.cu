90d0bb17155c1493cc3a5cc4d72920fde6c6b4cf
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-10-31 19:51:48 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -245,13 +245,13 @@ __device__ double cal_tnf_dist_d(double r1, double r2, float* TNF1, float* TNF2)
     return prob;
 }
 
-__global__ void get_tnf_graph(double* graph, float* TNF1, float* TNF2, double* contig_log, size_t nc1, size_t nc2, size_t off1,
-                              size_t off2) {
+__global__ void get_tnf_graph(double* graph, float* TNF, double* contig_log, size_t nc1, size_t nc2, size_t off1, size_t off2) {
     size_t prob_index = (threadIdx.x + blockIdx.x * blockDim.x);
     size_t r1 = prob_index / nc2;
     size_t r2 = prob_index % nc2;
     if (r1 >= nc1) return;
-    graph[prob_index] = 1. - cal_tnf_dist_d(contig_log[off1 + r1], contig_log[off2 + r2], TNF1 + r1 * 136, TNF2 + r2 * 136);
+    graph[prob_index] =
+        1. - cal_tnf_dist_d(contig_log[off1 + r1], contig_log[off2 + r2], TNF + (r1 + off1) * 136, TNF2 + (r2 + off2) * 136);
 }
 
 __global__ void get_tnf_max_prob_sample(double* __restrict__ max_dist, float* TNF, double* size_log, size_t* contigs, size_t nobs,
@@ -890,8 +890,8 @@ void gen_tnf_graph(Graph& g, Similarity cutoff) {
         for (size_t jj = 0; jj < nobs; jj += TILE) {
             {
                 size_t bloqs = ((TILE * TILE) + numThreads2 - 1) / numThreads2;
-                get_tnf_graph<<<numThreads2, bloqs>>>(graph_d, TNF_d + ii * 136, TNF_d + jj * 136, contig_log, min(TILE, (nobs - ii)),
-                                                      min(TILE, (nobs - jj)), ii, jj);
+                get_tnf_graph<<<numThreads2, bloqs>>>(graph_d, TNF_d, contig_log, min(TILE, (nobs - ii)), min(TILE, (nobs - jj)), ii,
+                                                      jj);
                 cudaDeviceSynchronize();
                 cudaMemcpy(graph_h, graph_d, TILE * TILE * sizeof(double), cudaMemcpyDeviceToHost);
             }
