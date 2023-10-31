6c3da1ef2233de3235349c7fa41ad05492abce96
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-10-31 19:40:50 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -888,7 +888,6 @@ void gen_tnf_graph(Graph& g, Similarity cutoff) {
         cudaMallocHost((void**)&graph_h, TILE * TILE * sizeof(double));
         std::vector<std::priority_queue<std::pair<size_t, double>, std::vector<std::pair<size_t, double>>, CompareEdge>> edges(TILE);
         for (size_t jj = 0; jj < nobs; jj += TILE) {
-            TIMERSTART(1);
             {
                 size_t bloqs = ((TILE * TILE) + numThreads2 - 1) / numThreads2;
                 get_tnf_graph<<<numThreads2, 1>>>(graph_d, TNF_d + ii * nTNF, TNF_d + jj * nTNF, contig_log, min(TILE, (nobs - ii)),
@@ -896,8 +895,6 @@ void gen_tnf_graph(Graph& g, Similarity cutoff) {
                 cudaDeviceSynchronize();
                 cudaMemcpy(graph_h, graph_d, TILE * TILE * sizeof(double), cudaMemcpyDeviceToHost);
             }
-
-            TIMERSTOP(1);
             for (size_t i = ii; i < ii + TILE && i < nobs; ++i) {
                 size_t que_index = i - ii;
                 for (size_t j = jj; j < jj + TILE && j < nobs; ++j) {
