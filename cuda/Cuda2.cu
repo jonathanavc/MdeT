ff77926b091cc265892054a1a72f189287690255
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-11-01 23:04:02 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -912,7 +912,7 @@ void gen_tnf_graph(Graph& g, Similarity cutoff) {
                 size_t que_index = i - ii;
                 for (size_t j = jj; j < jj + TILE && j < nobs; ++j) {
                     if (i == j || !is_nz(i, j)) continue;
-                    double sTNF = graph_h[(i - ii) * matrix_x + (j - jj)];
+                    double sTNF = graph_h[(i - ii) * TILE + (j - jj)];
                     /*
                     double sTNF2 = 1. - cal_tnf_dist(i, j);
                     if (abs(sTNF - sTNF2) >= 0.00001) {
