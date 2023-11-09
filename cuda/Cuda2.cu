5ec52fb970106ac6a189ccda302edc50c4b024e5
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-11-09 16:28:27 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -1641,10 +1641,9 @@ int main(int ac, char* av[]) {
                             }
                         }
                     }
-#pragma omp single
-                    for (int i = 0; t < numThreads; i++) {
-#pragma omp task
-                        if (i == omp_get_thread_num()) {
+                    for (int i = 0; i < numThreads; i++) {
+#pragma omp barrier
+                        if (i == t) {
                             for (auto& pair : contigs_l) {
                                 pair.second += nobs;
                             }
