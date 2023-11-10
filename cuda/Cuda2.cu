974c8a763ef266e1a10e45070dc96ef4fd69e496
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-11-10 14:04:27 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -571,6 +571,17 @@ void launch_tnf_max_prob_sample_kernel(std::vector<size_t> idx, double* max_dist
     getError("kernel");
 }
 
+void launch_tnf_max_prob_sample_kernel2(std::vector<size_t> idx, double* max_dist_d, double* max_dist_h, size_t _nobs) {
+    size_t* contigs_d;
+    cudaMalloc((void**)&contigs_d, idx.size() * sizeof(size_t));
+    cudaMemcpy(contigs_d, idx.data(), idx.size() * sizeof(size_t), cudaMemcpyHostToDevice);
+    get_tnf_max_prob_sample3<<<_nobs, numThreads2, numThreads2 * sizeof(double), streams[i]>>>(max_dist_d, TNF_d, contig_log,
+                                                                                               contigs_d, nobs, 0, _nobs);
+    cudaMemcpy(max_dist_h, max_dist_d, _nobs * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaFree(contigs_d);
+    getError("kernel");
+}
+
 void reader(int fpint, int id, size_t chunk, size_t _size, char* _mem) {
     size_t readSz = 0;
     while (readSz < _size) {
