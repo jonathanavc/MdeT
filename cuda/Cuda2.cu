e0dc07aec9d6d306407b55a6fbaded06e044cb29
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-12-02 19:43:26 -0300

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -1816,13 +1816,13 @@ int main(int ac, char* av[]) {
         }
         verbose_message("Finished reading %d contigs and %d coverages from %s\n", r, nABD, abdFile.c_str());
 
-        seqs.erase(std::remove(seqs.begin(), seqs.end(), ""), seqs.end());
+        seqs.erase(std::remove(seqs.begin(), seqs.end(), NULL), seqs.end());
         assert(nobs == seqs.size());
-        small_seqs.erase(std::remove(small_seqs.begin(), small_seqs.end(), ""), small_seqs.end());
+        small_seqs.erase(std::remove(small_seqs.begin(), small_seqs.end(), NULL), small_seqs.end());
         assert(nobs1 == small_seqs.size());
-        contig_names.erase(std::remove(contig_names.begin(), contig_names.end(), ""), contig_names.end());
+        contig_names.erase(std::remove(contig_names.begin(), contig_names.end(), NULL), contig_names.end());
         assert(nobs == contig_names.size());
-        small_contig_names.erase(std::remove(small_contig_names.begin(), small_contig_names.end(), ""), small_contig_names.end());
+        small_contig_names.erase(std::remove(small_contig_names.begin(), small_contig_names.end(), NULL), small_contig_names.end());
         assert(nobs1 == small_contig_names.size());
         logSizes.erase(std::remove(logSizes.begin(), logSizes.end(), -1), logSizes.end());
         assert(nobs == logSizes.size());
