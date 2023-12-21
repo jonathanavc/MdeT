3737d2aa24d26d87f56e4a054a22baeb05bf3b6a
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-12-21 15:10:03 -0300

    Update test.py

--- test.py
+++ test.py
@@ -303,7 +303,7 @@ tiempos["latex"] = {
     "Metabat2" : latex_m2,
     "MetabatCuda2" : latex_mc2,
     "Bins": latex_bins,
-    "aceleration": ((tiempos["Metabat2"]['Total']['avg']-tiempos["MetabatCuda2"]['Total']['avg']) / tiempos["Metabat2"]['Total']['avg']) * 100
+    "Aceleration": ((tiempos["Metabat2"]['Total']['avg']-tiempos["MetabatCuda2"]['Total']['avg']) / tiempos["Metabat2"]['Total']['avg']) * 100
 }
 
 print("Total CUDA: " + str(tiempos["MetabatCuda2"]['Total']['avg']))
