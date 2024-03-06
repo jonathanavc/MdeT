0f6e37b2a17199753143672a36d5e297e51fa4b4
Jonathan Venegas Caro <jonvenega@gmail.com>
2024-03-06 05:46:41 -0300

    Update verify.py

--- verify.py
+++ verify.py
@@ -2,12 +2,17 @@ import hashlib
 import os
 import sys
 
+test = False
+directory_path1 = ""
+directory_path2 = ""
+
 
 def check_md5(file1, file2):
     with open(file1, 'rb') as f1, open(file2, 'rb') as f2:
         file1_hash = hashlib.md5(f1.read()).hexdigest()
         file2_hash = hashlib.md5(f2.read()).hexdigest()
-
+        print("File1: " + file1_hash)
+        print("File2: " + file2_hash)
     return file1_hash == file2_hash
 
 def check_all_files_in_directory(directory_path1, directory_path2):
@@ -16,21 +21,22 @@ def check_all_files_in_directory(directory_path1, directory_path2):
     for i in range(1, len(files)):
         file1 = os.path.join(directory_path1, files[i])
         file2 = os.path.join(directory_path2, files[i])
-        if not check_md5(file1, file2):
-            print(f"The files {file1} and {file2} are not identical.")
-            break
-        verified_files_count += 1
+        if check_md5(file1, file2):
+            verified_files_count += 1
 
-    if verified_files_count == len(files):
-        print(f"All files are identical. Total files verified: {verified_files_count}")
+    if len(files) != 0:
+        return (verified_files_count / len(files)) * 100
+    else:
+        return 100
 
 
-directory_path1 = ""
-directory_path2 = ""
+
 if(len(sys.argv) < 3):
     print("Usage: python verify.py <directory_path1> <directory_path2>")
     exit()
 else:
+    if len(sys.argv) == 4 and (sys.argv[3] == "-t" or sys.argv[3] == "-T"):
+        test = True
     directory_path1 = sys.argv[1]
     directory_path2 = sys.argv[2]
-check_all_files_in_directory(directory_path1, directory_path2)
+    print(check_all_files_in_directory(directory_path1, directory_path2))
