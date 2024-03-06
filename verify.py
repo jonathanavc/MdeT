9b1f984fbb43c4da00eef766e56b2933c86331b4
Jonathan Venegas Caro <jonvenega@gmail.com>
2024-03-06 05:22:56 -0300

    Create verify.py

---
+++ verify.py
@@ -0,0 +1,30 @@
+import hashlib
+import os
+
+
+def check_md5(file1, file2):
+    with open(file1, 'rb') as f1, open(file2, 'rb') as f2:
+        file1_hash = hashlib.md5(f1.read()).hexdigest()
+        file2_hash = hashlib.md5(f2.read()).hexdigest()
+
+    return file1_hash == file2_hash
+
+def check_all_files_in_directory(directory_path1, directory_path2):
+    files = os.listdir(directory_path)
+    for j in range(i+1, len(files)):
+        file1 = os.path.join(directory_path, files[i])
+        file2 = os.path.join(directory_path2, files[i])
+        if not check_md5(file1, file2):
+            print(f"The files {file1} and {file2} are not identical.")
+            break
+
+def main():
+    directory_path1 = ""
+    directory_path2 = ""
+    if(len(sys.argv) < 3):
+        print("Usage: python verify.py <directory_path1> <directory_path2>")
+        exit()
+    else:
+        directory_path1 = sys.argv[1]
+        directory_path2 = sys.argv[2]
+    check_all_files_in_directory(directory_path1, directory_path2)
