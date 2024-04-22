f09d77d13548f29f3a191765df08fa786341f08c
Jonathan Venegas Caro <65467389+jonathanavc@users.noreply.github.com>
2024-04-21 22:47:07 -0400

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -3,6 +3,6 @@
 #nvcc ./cuda/TNF2.cu -lz -O3 -arch=native -o cuda2_ex
 #nvcc ./cuda/TNF3.cu -lz -std=c++17 -O3 -arch=native -Xcompiler="-pthread" -o cuda3_ex
 #nvcc ./cuda/Cuda.cu -std=c++17 -lboost_program_options -lboost_filesystem -lboost_graph -lboost_serialization -lboost_system -O3 -arch=native -lineinfo -use_fast_math -Xcompiler="-march=native -pthread -fopenmp" -o metabatcuda
-nvcc -g -G ./cuda/Cuda2.cu --default-stream per-thread -std=c++17 -lboost_program_options -lboost_filesystem -lboost_graph -lboost_serialization -lboost_system -O3 -arch=native -lineinfo -Xcompiler="-march=native -pthread -fopenmp" -o metabatcuda2
+nvcc ./cuda/Cuda2.cu --default-stream per-thread -std=c++17 -lboost_program_options -lboost_filesystem -lboost_graph -lboost_serialization -lboost_system -O3 -arch=native -lineinfo -Xcompiler="-march=native -pthread -fopenmp" -o metabatcuda2
 #nvcc ./cuda/metabatcuda2.cu --default-stream per-thread -std=c++17 -lboost_program_options -lboost_filesystem -lboost_graph -lboost_serialization -lboost_system -O3 -arch=native -lineinfo -use_fast_math -lz -Xcompiler="-march=native -pthread -fopenmp" -o metabatcuda2t
 #g++ ./extra/comp.cpp -lz -fopenmp -O3 -o comp_ex
