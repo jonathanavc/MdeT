3e87e277515db7bd947937587ec0f08128f197ff
Jonathan Venegas Caro <65467389+jonathanavc@users.noreply.github.com>
2024-04-22 18:05:21 -0400

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -965,9 +965,18 @@ size_t gen_tnf_graph_sample(double coverage = 1., bool full = false) {
     random_unique(idx.begin(), idx.end(), _nobs);
 
     double *max_nobs_h;
+    double *max_nobs_h2;
     cudaMallocHost((void**)&max_nobs_h, _nobs * sizeof(double));
+    cudaMallocHost((void**)&max_nobs_h2, _nobs * sizeof(double));
     //cudaMalloc((void**)&max_nobs_d, _nobs * sizeof(double));
     launch_tnf_max_prob_sample_kernel_multi(idx, max_nobs_h, _nobs);
+    launch_tnf_max_prob_sample_kernel(idx, max_nobs_h, _nobs);
+    for (size_t i = 0; i < _nobs; i++) {
+        if (max_nobs_h[i] != max_nobs_h2[i]) {
+            printf("Ctg: %d, max_nobs_h:%f, max_nobs_h2:%f\n", i, max_nobs_h[i], max_nobs_h2[i]);
+            exit(1);
+        }
+    }
     getError("launch_tnf_max_prob_sample_kernel");
     //cudaFree(max_nobs_d);
 
