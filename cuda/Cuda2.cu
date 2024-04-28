ff149a21124d93d9b207f4764648cc5461331196
Jonathan Venegas Caro <65467389+jonathanavc@users.noreply.github.com>
2024-04-28 19:05:14 -0400

    Update Cuda2.cu

--- cuda/Cuda2.cu
+++ cuda/Cuda2.cu
@@ -314,7 +314,7 @@ __global__ void get_tnf_graph(double* graph, const float* __restrict__ TNF, cons
     size_t ct1 = off1 + r1;
     size_t ct2 = off2 + r2;
     if (ct1 == ct2) return;
-    double preProb = cal_tnf_pre_dist_d(contig_log[ct1], contig_log[ct2], TNF + ct1 * 136, TNF + ct2 * 136);
+    double preProb = cal_tnf_pre_dist_d_if(contig_log[ct1], contig_log[ct2], TNF + ct1 * 136, TNF + ct2 * 136);
 
     if (preProb > floor_preProb_cutoff)
         graph[prob_index] = 1. - (1. / (1. + exp(preProb)));
